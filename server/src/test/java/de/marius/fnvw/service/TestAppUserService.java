e2eab3215b38f917616b4d9bcc4aff23caaae617
Marius Damm <mariusdamm1010@gmail.com>
2024-08-17 13:13:56 +0200

    test (backend): add test file for AppUserService
    Adds the TestAppUserService.java file which will contain the integration tests for AppUserService
    
    Issue #1

---
+++ server/src/test/java/de/marius/fnvw/service/TestAppUserService.java
@@ -0,0 +1,15 @@
+package de.marius.fnvw.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class TestAppUserService {
+
+    @BeforeEach
+    @Transactional
+    void set_up() {
+        // still empty
+    }
+}
