0950bc1cdf6251eb9464893b01daac01ed91f1f2
Marius Damm <mariusdamm1010@gmail.com>
2024-08-17 14:07:16 +0200

    test (backend): add tests for typeBelongsToUser()
    Add tests for the method typeBelongsToUser() in AppUserService. One test tests with a type belonging to the user and the other test tests with a type not belonging to the user.
    
    Issue #1

--- server/src/test/java/de/marius/fnvw/service/TestAppUserService.java
+++ server/src/test/java/de/marius/fnvw/service/TestAppUserService.java
@@ -3,10 +3,13 @@ package de.marius.fnvw.service;
 import de.marius.fnvw.dao.AppUserRepository;
 import de.marius.fnvw.dao.RoleRepository;
 import de.marius.fnvw.dto.EntryGroupDto;
+import de.marius.fnvw.dto.EntryTypeDto;
 import de.marius.fnvw.entity.AppUser;
 import de.marius.fnvw.entity.EntryGroup;
+import de.marius.fnvw.entity.EntryType;
 import de.marius.fnvw.entity.Role;
 import de.marius.fnvw.exception.DataNotFoundException;
+import de.marius.fnvw.exception.ForbiddenDataException;
 import de.marius.fnvw.exception.MissingDataException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,14 +37,18 @@ class TestAppUserService {
     private EntryGroupService entryGroupService;
     @Autowired
     private AppUserService appUserService;
+    @Autowired
+    private EntryTypeService entryTypeService;
 
     private AppUser testUser1;
     private EntryGroup groupOfUser1;
     private EntryGroup groupOfUser2;
+    private EntryType typeOfUser1;
+    private EntryType typeOfUser2;
 
     @BeforeEach
     @Transactional
-    void set_up() throws DataNotFoundException, MissingDataException {
+    void set_up() throws DataNotFoundException, MissingDataException, ForbiddenDataException {
         Role role = roleRepository.findByAuthority("USER").orElseGet(() -> roleRepository.save(new Role("USER")));
         List<Role> roles = new ArrayList<>();
         roles.add(role);
@@ -59,14 +66,22 @@ class TestAppUserService {
         String testEntryGroupName1 = "Test Groupname1";
         int testEntryGroupMonth1 = 202409;
         boolean tesEntryGroupIsIntake1 = true;
-        EntryGroupDto dto1 = new EntryGroupDto(testEntryGroupName1, testEntryGroupMonth1, tesEntryGroupIsIntake1);
-        groupOfUser1 = entryGroupService.addEntryGroup(dto1, testUser1);
-
+        EntryGroupDto groupDto1 = new EntryGroupDto(testEntryGroupName1, testEntryGroupMonth1, tesEntryGroupIsIntake1);
         String testEntryGroupName2 = "Test Groupname2";
         int testEntryGroupMonth2 = 202409;
         boolean tesEntryGroupIsIntake2 = false;
-        EntryGroupDto dto2 = new EntryGroupDto(testEntryGroupName2, testEntryGroupMonth2, tesEntryGroupIsIntake2);
-        groupOfUser2 = entryGroupService.addEntryGroup(dto2, testUser2);
+        EntryGroupDto groupDto2 = new EntryGroupDto(testEntryGroupName2, testEntryGroupMonth2, tesEntryGroupIsIntake2);
+
+        groupOfUser1 = entryGroupService.addEntryGroup(groupDto1, testUser1);
+        groupOfUser2 = entryGroupService.addEntryGroup(groupDto2, testUser2);
+
+        String testEntryTypeName1 = "test entrytype1";
+        EntryTypeDto typeDto1 = new EntryTypeDto(groupOfUser1.getId(), testEntryTypeName1);
+        String testEntryTypeName2 = "test entrytype2";
+        EntryTypeDto typeDto2 = new EntryTypeDto(groupOfUser2.getId(), testEntryTypeName2);
+
+        typeOfUser1 = entryTypeService.addEntryType(typeDto1, testUser1);
+        typeOfUser2 = entryTypeService.addEntryType(typeDto2, testUser2);
     }
 
     @Test
@@ -82,6 +97,20 @@ class TestAppUserService {
     void test_invalid_group_belongs_to_user() throws DataNotFoundException {
         boolean result = appUserService.groupBelongsToUser(testUser1, groupOfUser2.getId());
 
+        assertFalse(result);
+    }
+
+    @Test
+    @Transactional
+    void test_valid_type_belongs_to_user() throws DataNotFoundException {
+        boolean result = appUserService.typeBelongsToUser(testUser1, typeOfUser1.getId());
+        assertTrue(result);
+    }
+
+    @Test
+    @Transactional
+    void test_invalid_type_belongs_to_user() throws DataNotFoundException {
+        boolean result = appUserService.typeBelongsToUser(testUser1, typeOfUser2.getId());
         assertFalse(result);
     }
 }
