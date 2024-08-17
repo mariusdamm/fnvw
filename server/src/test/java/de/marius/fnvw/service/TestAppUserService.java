b2630cf30d1a78f3011c4903005ae14a2ae5bbba
Marius Damm <mariusdamm1010@gmail.com>
2024-08-17 13:57:27 +0200

    ref (tests backend): move entity initialization to set_up()
    Move the initialization of the entry groups into the set_up() method in TestAppUserService.java.
    
    Issue #1

--- server/src/test/java/de/marius/fnvw/service/TestAppUserService.java
+++ server/src/test/java/de/marius/fnvw/service/TestAppUserService.java
@@ -32,57 +32,55 @@ class TestAppUserService {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private EntryGroupService entryGroupService;
-
-    private AppUser testUser;
     @Autowired
     private AppUserService appUserService;
 
+    private AppUser testUser1;
+    private EntryGroup groupOfUser1;
+    private EntryGroup groupOfUser2;
+
     @BeforeEach
     @Transactional
-    void set_up() {
+    void set_up() throws DataNotFoundException, MissingDataException {
         Role role = roleRepository.findByAuthority("USER").orElseGet(() -> roleRepository.save(new Role("USER")));
         List<Role> roles = new ArrayList<>();
         roles.add(role);
 
-        String testUserName = "Test User";
-        String testUserUsername = "Test Username";
-        String testUserPwd = "test_pwd";
+        String testUserName1 = "Test User1";
+        String testUserUsername1 = "Test Username1";
+        String testUserPwd1 = "test_pwd1";
+        String testUserName2 = "Test User2";
+        String testUserUsername2 = "Test Username2";
+        String testUserPwd2 = "test_pwd2";
 
-        testUser = appUserRepository.save(new AppUser(testUserName, testUserUsername, passwordEncoder.encode(testUserPwd), roles));
+        testUser1 = appUserRepository.save(new AppUser(testUserName1, testUserUsername1, passwordEncoder.encode(testUserPwd1), roles));
+        AppUser testUser2 = appUserRepository.save(new AppUser(testUserName2, testUserUsername2, passwordEncoder.encode(testUserPwd2), roles));
+
+        String testEntryGroupName1 = "Test Groupname1";
+        int testEntryGroupMonth1 = 202409;
+        boolean tesEntryGroupIsIntake1 = true;
+        EntryGroupDto dto1 = new EntryGroupDto(testEntryGroupName1, testEntryGroupMonth1, tesEntryGroupIsIntake1);
+        groupOfUser1 = entryGroupService.addEntryGroup(dto1, testUser1);
+
+        String testEntryGroupName2 = "Test Groupname2";
+        int testEntryGroupMonth2 = 202409;
+        boolean tesEntryGroupIsIntake2 = false;
+        EntryGroupDto dto2 = new EntryGroupDto(testEntryGroupName2, testEntryGroupMonth2, tesEntryGroupIsIntake2);
+        groupOfUser2 = entryGroupService.addEntryGroup(dto2, testUser2);
     }
 
     @Test
     @Transactional
-    void test_valid_group_belongs_to_user() throws DataNotFoundException, MissingDataException {
-        String testEntryGroupName = "Test Groupname";
-        int testEntryGroupMonth = 202409;
-        boolean tesEntryGroupIsIntake = true;
-        EntryGroupDto dto = new EntryGroupDto(testEntryGroupName, testEntryGroupMonth, tesEntryGroupIsIntake);
-        EntryGroup group = entryGroupService.addEntryGroup(dto, testUser);
-
-        boolean result = appUserService.groupBelongsToUser(testUser, group.getId());
+    void test_valid_group_belongs_to_user() throws DataNotFoundException {
+        boolean result = appUserService.groupBelongsToUser(testUser1, groupOfUser1.getId());
 
         assertTrue(result);
     }
 
     @Test
     @Transactional
-    void test_invalid_group_belongs_to_user() throws DataNotFoundException, MissingDataException {
-        Role role = roleRepository.findByAuthority("USER").orElseGet(() -> roleRepository.save(new Role("USER")));
-        List<Role> roles = new ArrayList<>();
-        roles.add(role);
-        String testUserName = "Test User2";
-        String testUserUsername = "Test Username2";
-        String testUserPwd = "test_pwd2";
-        AppUser testUser2 = appUserRepository.save(new AppUser(testUserName, testUserUsername, passwordEncoder.encode(testUserPwd), roles));
-
-        String testEntryGroupName = "Test Groupname";
-        int testEntryGroupMonth = 202409;
-        boolean tesEntryGroupIsIntake = true;
-        EntryGroupDto dto = new EntryGroupDto(testEntryGroupName, testEntryGroupMonth, tesEntryGroupIsIntake);
-        EntryGroup group = entryGroupService.addEntryGroup(dto, testUser2);
-
-        boolean result = appUserService.groupBelongsToUser(testUser, group.getId());
+    void test_invalid_group_belongs_to_user() throws DataNotFoundException {
+        boolean result = appUserService.groupBelongsToUser(testUser1, groupOfUser2.getId());
 
         assertFalse(result);
     }
