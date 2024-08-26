1f1c58f6f547ba3bb433c6106e97e028b4f935ec
Marius Damm <mariusdamm1010@gmail.com>
2024-08-26 23:37:32 +0200

    chore (backend): enhance logging in AppUserController
    Enhance logging in AppUserController by using the LogInfo class.
    
    Issue #9

--- server/src/main/java/de/marius/fnvw/controller/AppUserController.java
+++ server/src/main/java/de/marius/fnvw/controller/AppUserController.java
@@ -1,8 +1,11 @@
 package de.marius.fnvw.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import de.marius.fnvw.dto.AppUserDto;
 import de.marius.fnvw.entity.AppUser;
 import de.marius.fnvw.service.AppUserService;
+import de.marius.fnvw.util.LogInfo;
+import de.marius.fnvw.util.LogLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,6 +14,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/user")
@@ -24,17 +29,23 @@ public class AppUserController {
     }
 
     @GetMapping("/self")
-    public ResponseEntity<AppUserDto> getSelf() {
+    public ResponseEntity<AppUserDto> getSelf() throws JsonProcessingException {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        logger.debug("Username {} retrieved - Method: getSelf - Action: Getting username from the security context", username);
+        logger.debug(
+                LogInfo.toJson(LogLevel.DEBUG, "AppUserController.getSelf", new Date(), "", "", "username " + username + " extracted from SecurityContext", username)
+        );
 
         AppUser user = appUserService.getUserByUsername(username);
         if (user == null) {
-            logger.error("User {} not found - Method: getSelf - Reason: The username does not exist in the database - Action: return HttpStatus Not_Found and data null", username);
+            logger.error(
+                    LogInfo.toJson(LogLevel.ERROR, "AppUserController.getSelf", new Date(), "user is null", "User " + username + " not found in the database", "return HttpStatus NOT_FOUND and data null", username)
+            );
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
 
-        logger.info("User {} retrieved - Method: getSelf - Action: return HttpStatus Ok and data UserDto of user", username);
+        logger.info(
+                LogInfo.toJson(LogLevel.INFO, "AppUserController.getSelf", new Date(), "User " + username + " retrieved", "", "return HttpStatus OK and data UserDto of the user", username)
+        );
         return ResponseEntity.status(HttpStatus.OK).body(user.toDto());
     }
 }
