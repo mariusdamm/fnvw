3e6d7b20197fcafb229fdc470bf8154095f33b1f
Marius Damm <mariusdamm1010@gmail.com>
2024-08-30 18:00:11 +0200

    chore (backend): add logging in EntryTypeController
    Add logging to the EntryTypeController class. Make use of the LogInfo helper class
    
    Issue #9

--- server/src/main/java/de/marius/fnvw/controller/EntryTypeController.java
+++ server/src/main/java/de/marius/fnvw/controller/EntryTypeController.java
@@ -1,5 +1,6 @@
 package de.marius.fnvw.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import de.marius.fnvw.dto.EntryTypeDto;
 import de.marius.fnvw.entity.AppUser;
 import de.marius.fnvw.entity.EntryType;
@@ -8,6 +9,10 @@ import de.marius.fnvw.exception.ForbiddenDataException;
 import de.marius.fnvw.exception.MissingDataException;
 import de.marius.fnvw.service.AppUserService;
 import de.marius.fnvw.service.EntryTypeService;
+import de.marius.fnvw.util.LogInfo;
+import de.marius.fnvw.util.LogLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +24,7 @@ public class EntryTypeController {
 
     private final EntryTypeService entryTypeService;
     private final AppUserService appUserService;
+    private final Logger logger = LoggerFactory.getLogger(EntryTypeController.class);
 
     public EntryTypeController(EntryTypeService entryTypeService, AppUserService appUserService) {
         this.entryTypeService = entryTypeService;
@@ -26,20 +32,22 @@ public class EntryTypeController {
     }
 
     @PostMapping("")
-    public ResponseEntity<EntryTypeDto> createEntryType(@RequestBody EntryTypeDto body) {
+    public ResponseEntity<EntryTypeDto> createEntryType(@RequestBody EntryTypeDto body) throws JsonProcessingException {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        logger.debug(LogInfo.toJson(LogLevel.DEBUG, "EntryTypeController.createEntryType", "", "", "Username retrieved", username));
         AppUser user = appUserService.getUserByUsername(username);
         try {
             EntryType type = entryTypeService.addEntryType(body, user);
+            logger.info(LogInfo.toJson(LogLevel.INFO, "EntryTypeController.createEntryType", "", "", "EntryType created with id " + type.getId() + ". Return HttpStatus CREATED and data entryType as EntryTypeDto", username));
             return ResponseEntity.status(HttpStatus.CREATED).body(type.toDto());
         } catch (MissingDataException e) {
-            e.printStackTrace();
+            logger.warn(LogInfo.toJson(LogLevel.WARNING, "EntryTypeController.createEntryType", e.getMessage(), "No EntryTypeDto was provided by the user", "Return HttpStatus BAD_REQUEST and data null", username));
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         } catch (DataNotFoundException e) {
-            e.printStackTrace();
+            logger.error(LogInfo.toJson(LogLevel.ERROR, "EntryTypeController.createEntryType", e.getMessage(), "The username/entrygroup does not exist in the database", "Return HttpStatus NOT_FOUND and data null", username));
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } catch (ForbiddenDataException e) {
-            e.printStackTrace();
+            logger.warn(LogInfo.toJson(LogLevel.WARNING, "EntryTypeController.createEntryType", e.getMessage(), "The entryType does not belong to the user", "Return HttpStatus FORBIDDEN and data null", username));
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
         }
     }
