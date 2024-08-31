eeb411ccccfa8b7258ca454013afbb2015f8eb43
Marius Damm <mariusdamm1010@gmail.com>
2024-08-31 20:11:37 +0200

    chore (backend): add logging in EntryTypeService
    Add logging in EntryTypeService.java and make use of the LogInfo class.
    
    Issue #9

--- server/src/main/java/de/marius/fnvw/service/EntryTypeService.java
+++ server/src/main/java/de/marius/fnvw/service/EntryTypeService.java
@@ -9,6 +9,10 @@ import de.marius.fnvw.entity.EntryType;
 import de.marius.fnvw.exception.DataNotFoundException;
 import de.marius.fnvw.exception.ForbiddenDataException;
 import de.marius.fnvw.exception.MissingDataException;
+import de.marius.fnvw.util.LogInfo;
+import de.marius.fnvw.util.LogLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +22,7 @@ public class EntryTypeService {
 
     private final EntryTypeRepository entryTypeRepository;
     private final EntryGroupRepository entryGroupRepository;
+    private final Logger logger = LoggerFactory.getLogger(EntryTypeService.class);
 
     public EntryTypeService(EntryTypeRepository entryTypeRepository, EntryGroupRepository entryGroupRepository) {
         this.entryTypeRepository = entryTypeRepository;
@@ -26,27 +31,31 @@ public class EntryTypeService {
 
     public EntryType addEntryType(EntryTypeDto body, AppUser user) throws MissingDataException,
             DataNotFoundException, ForbiddenDataException {
+        if (user == null) {
+            logger.error(LogInfo.toJson(LogLevel.ERROR, "EntryTypeService.addEntryType", "AppUser user is null", "AppUser user passed by calling function is null", "Throw MissingDataException", ""));
+            throw new DataNotFoundException("User could not be found in the database");
+        }
+
         if (body == null) {
+            logger.warn(LogInfo.toJson(LogLevel.WARNING, "EntryTypeService.addEntryType", "EntryType body is null", "EntryType passed by calling function is null", "Throw MissingDataException", user.getUsername()));
             throw new MissingDataException("Entry body is null");
         }
         EntryType entryType = body.toEntryType();
 
-        if (user == null) {
-            throw new DataNotFoundException("User could not be found in the database");
-        }
-        List<EntryGroup> groups = entryGroupRepository.findByOwner(user);
+        List<EntryGroup> allGroupsOfUser = entryGroupRepository.findByOwner(user);
         EntryGroup group = entryGroupRepository.findById(body.getEntryGroupId()).orElse(null);
         if (group == null) {
-            throw new DataNotFoundException("EntryGroup with ID " + body.getEntryGroupId() +
-                    " could not be found in the database");
+            logger.error(LogInfo.toJson(LogLevel.ERROR, "EntryTypeService.addEntryType", "EntryGroup group is null", "EntryGroup with id " + body.getEntryGroupId() + " could not be found in the database", "Throw DataNotFoundException", user.getUsername()));
+            throw new DataNotFoundException("EntryGroup with ID " + body.getEntryGroupId() + " could not be found in the database");
         }
-        if (!groups.contains(group)) {
-            throw new ForbiddenDataException("EntryType with ID " + body.getEntryGroupId() +
-                    " does not belong to user " + user.getName());
+        if (!allGroupsOfUser.contains(group)) {
+            logger.warn(LogInfo.toJson(LogLevel.WARNING, "EntryTypeService.addEntryType", "allGroupsOfUser does not contain group with id " + group.getId(), "EntryGroup does not belong to user", "Throw ForbiddenDataException", user.getUsername()));
+            throw new ForbiddenDataException("EntryType with ID " + body.getEntryGroupId() + " does not belong to user " + user.getName());
         }
         entryType.setGroup(group);
 
         entryTypeRepository.save(entryType);
+        logger.info(LogInfo.toJson(LogLevel.INFO, "EntryTypeService.addEntryType", "", "", "Entry type added successfully", user.getUsername()));
         return entryType;
     }
 }
