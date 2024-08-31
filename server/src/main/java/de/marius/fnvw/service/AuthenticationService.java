03f8a41d66e0ffea5ba810dc212ed0ce59125376
Marius Damm <mariusdamm1010@gmail.com>
2024-08-31 18:12:19 +0200

    chore (backend): add logging in AuthenticationService
    Add logging in AuthenticationService.java and make use of the LogInfo class.
    
    Issue #9

--- server/src/main/java/de/marius/fnvw/service/AuthenticationService.java
+++ server/src/main/java/de/marius/fnvw/service/AuthenticationService.java
@@ -8,6 +8,10 @@ import de.marius.fnvw.dto.RegisterDto;
 import de.marius.fnvw.entity.AppUser;
 import de.marius.fnvw.entity.Role;
 import de.marius.fnvw.exception.ConstraintException;
+import de.marius.fnvw.util.LogInfo;
+import de.marius.fnvw.util.LogLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -24,7 +28,7 @@ public class AuthenticationService {
     private final AppUserRepository appUserRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
-
+    private final Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
 
     public AuthenticationService(AuthenticationManager authenticationManager, TokenService tokenService, AppUserRepository appUserRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
@@ -35,20 +39,25 @@ public class AuthenticationService {
     }
 
     public LoginResponseDto loginUser(LoginDto body) {
+        logger.debug(LogInfo.toJson(LogLevel.DEBUG, "AuthenticationService.loginUser", "", "", "Attempting to authenticate user", body.getUsername()));
         Authentication auth = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(body.getUsername(), body.getPassword()));
 
+        logger.info(LogInfo.toJson(LogLevel.INFO, "AuthenticationService.loginUser", "", "", "User authenticated successfully. Token generated", body.getUsername()));
         String token = tokenService.generateJwt(auth);
         return new LoginResponseDto(token);
     }
 
     public void registerUser(RegisterDto body) throws ConstraintException {
+        logger.debug(LogInfo.toJson(LogLevel.DEBUG, "AuthenticationService.registerUser", "", "", "Attempting to register user", body.getUsername()));
         if (body.getUsername().isBlank() || body.getPassword().isBlank() || body.getName().isBlank()) {
+            logger.warn(LogInfo.toJson(LogLevel.WARNING, "AuthenticationService.registerUser", "IllegalArgumentException", "Username, password or name is blank", "Throw IllegalArgumentException", body.getUsername()));
             throw new IllegalArgumentException("Username, password and name must not be empty");
         }
 
         if (appUserRepository.findByUsername(body.getUsername()).isPresent()) {
-            throw new ConstraintException("Username already taken");
+            logger.warn(LogInfo.toJson(LogLevel.WARNING, "AuthenticationService.registerUser", "Username already present in the Database", "Username already taken", "Throw ConstraintException", body.getUsername()));
+            throw new ConstraintException("Username " + body.getUsername() + " is already taken");
         }
 
         Role userRole = roleRepository.findByAuthority("USER").orElseGet(() -> roleRepository.save(new Role("USER")));
@@ -57,5 +66,6 @@ public class AuthenticationService {
         appUserRepository.save(new AppUser(
                 body.getName(), body.getUsername(), passwordEncoder.encode(body.getPassword()), roles
         ));
+        logger.info(LogInfo.toJson(LogLevel.INFO, "AuthenticationService.registerUser", "", "", "User registered successfully", body.getUsername()));
     }
 }
