c377730bfc3cdd1818572606d65d3955923b871c
Marius Damm <marius.damm@dfs.de>
2024-09-13 11:53:18 +0200

    perf (scripts): update the test script
    Improve performance of Dockerfile, docker-compose file and the shell script
    
    Issue #18

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-docker compose -f ../server/docker-compose.test.yml up --build -d
+
+docker compose -f ../server/docker-compose.test.yml up -d
 docker logs -f fnvw-app-test
 docker wait fnvw-app-test
 docker compose -f ../server/docker-compose.test.yml down
