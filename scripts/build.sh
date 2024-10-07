a499c6f3c2c922423bf28e3a6a1fc4bafb1b7395
Marius Damm <45565475+Mariusdamm@users.noreply.github.com>
2024-10-07 23:04:15 +0200

    ci: Update build.sh
    Add mkdir static to build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker compose -f ../web/docker-compose.build.yml up || exit
-rm -rf ../server/src/main/resources/static/* || exit
+rm -rf ../server/src/main/resources/static/ || exit
+mkdir ../server/src/main/resources/static
 cp -r ../web/dist/fnvw-frontend/browser/* ../server/src/main/resources/static/ || exit
 docker compose -f ../server/docker-compose.build.yml up || exit
