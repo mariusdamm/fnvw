fec012324074746cf5261619d98529fe17ac334c
Marius Damm <mariusdamm1010@gmail.com>
2024-10-25 17:35:03 +0200

    chore (web): extract entrygroup cards to component
    Issue #28

--- web/src/app/screens/entry-screen/entrygroup-card/entrygroup-card.component.ts
+++ web/src/app/screens/entry-screen/entrygroup-card/entrygroup-card.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {EntrygroupDto} from "../../../dtos/entrygroup-dto";
+import {EntryCreateModalComponent} from "../entry-create-modal/entry-create-modal.component";
+import {UtilService} from "../../../services/util.service";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-entrygroup-card',
   standalone: true,
-  imports: [],
+  imports: [
+    EntryCreateModalComponent,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './entrygroup-card.component.html',
   styleUrl: './entrygroup-card.component.css'
 })
 export class EntrygroupCardComponent {
 
+  @Input() group?: EntrygroupDto;
+
+  constructor(protected utilService: UtilService) {
+  }
 }
