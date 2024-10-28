79032eb99d56935aa626755440b479cf45bba3ca
Marius Damm <mariusdamm1010@gmail.com>
2024-10-28 08:58:44 +0100

    chore (web): add expanded icon
    Add expanded icon and rotate functionality
    
    Issue #28

--- web/src/app/screens/entry-screen/entrygroup-card/entrygroup-card.component.ts
+++ web/src/app/screens/entry-screen/entrygroup-card/entrygroup-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {EntrygroupDto} from "../../../dtos/entrygroup-dto";
 import {EntryCreateModalComponent} from "../entry-create-modal/entry-create-modal.component";
 import {UtilService} from "../../../services/util.service";
@@ -17,8 +17,20 @@ import {NgForOf, NgIf} from "@angular/common";
 })
 export class EntrygroupCardComponent {
 
+  @ViewChild('collapseIcon') collapseIcon!: ElementRef;
   @Input() group?: EntrygroupDto;
 
   constructor(protected utilService: UtilService) {
+  }
+
+  toggleIconRotate() {
+    if (this.collapseIcon.nativeElement.classList.contains('svg-rotate')){
+      this.collapseIcon.nativeElement.classList.remove('svg-rotate');
+      this.collapseIcon.nativeElement.classList.add('svg-unrotate');
+    }
+    else {
+      this.collapseIcon.nativeElement.classList.remove('svg-unrotate');
+      this.collapseIcon.nativeElement.classList.add('svg-rotate');
+    }
   }
 }
