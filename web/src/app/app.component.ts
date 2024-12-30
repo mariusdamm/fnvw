cdc01b621e208c3e19d4bafc1099ab809120b7f5
Marius Damm <mariusdamm1010@gmail.com>
2024-12-30 18:01:50 +0100

    ref (web): refactor app component
    Remove unused stuff
    
    Issue #35

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {NgIf, NgOptimizedImage} from "@angular/common";
-import {LoginScreenComponent} from "./screens/login-screen/login-screen.component";
-import {RegisterScreenComponent} from "./screens/register-screen/register-screen.component";
 import {HelpModalComponent} from "./header/help-modal/help-modal.component";
 import {NameDropdownComponent} from "./header/name-dropdown/name-dropdown.component";
 import {AuthService} from "./services/auth.service";
@@ -11,7 +9,7 @@ import {Subscription} from "rxjs";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, LoginScreenComponent, RegisterScreenComponent, HelpModalComponent, RouterLink, NameDropdownComponent, NgIf],
+  imports: [RouterOutlet, NgOptimizedImage, HelpModalComponent, RouterLink, NameDropdownComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,7 +19,7 @@ export class AppComponent implements OnInit, OnDestroy {
   private loggedInSubscription?: Subscription;
 
   constructor(
-    private authService: AuthService,
+    private readonly authService: AuthService,
   ) {
   }
 
