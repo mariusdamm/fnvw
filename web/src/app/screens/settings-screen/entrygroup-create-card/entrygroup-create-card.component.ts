641871a35f75d4d5b8dfe2a3b0adc5cf6710bf3b
Marius Damm <mariusdamm1010@gmail.com>
2024-12-31 17:17:39 +0100

    feat (web): implement entrygroup create card
    Issue #35

--- web/src/app/screens/settings-screen/entrygroup-create-card/entrygroup-create-card.component.ts
+++ web/src/app/screens/settings-screen/entrygroup-create-card/entrygroup-create-card.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {EntrygroupCreateModalComponent} from "../entrygroup-create-modal/entrygroup-create-modal.component";
+import {EntrygroupDto} from "../../../dtos/entrygroup-dto";
+import {AxiosService} from "../../../services/axios.service";
+import {AuthService} from "../../../services/auth.service";
+import {toggleIconRotate} from "../../../util";
 
 @Component({
   selector: 'app-entrygroup-create-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    EntrygroupCreateModalComponent
+  ],
   templateUrl: './entrygroup-create-card.component.html',
   styleUrl: './entrygroup-create-card.component.css'
 })
-export class EntrygroupCreateCardComponent {
+export class EntrygroupCreateCardComponent implements OnInit{
+  groups: EntrygroupDto[] = []
+  @ViewChild('collapseIcon') collapseIcon!: ElementRef;
 
+  ngOnInit() {
+    this.fetchGroups();
+  }
+
+  constructor(
+    private readonly axiosService: AxiosService,
+    private readonly authService: AuthService,
+  ) {
+  }
+
+  fetchGroups() {
+    this.axiosService.request(
+      "GET",
+      "/entrygroup",
+      ""
+    ).then(response => {
+      return response.data;
+    }).then(groups => {
+      if (groups !== null)
+        this.groups = groups;
+    }).catch(error => {
+      if (error.response.status === 401)
+        this.authService.deleteJwtToken();
+    });
+  }
+
+  protected readonly toggleIconRotate = toggleIconRotate;
 }
