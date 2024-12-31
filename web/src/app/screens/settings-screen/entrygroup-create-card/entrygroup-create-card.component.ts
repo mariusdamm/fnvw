221d3e14ef9daa7340863728a0960929390ae316
Marius Damm <mariusdamm1010@gmail.com>
2024-12-31 16:51:01 +0100

    feat (web): add entrygroup-create-card
    Not implemented yet
    
    Issue #35

---
+++ web/src/app/screens/settings-screen/entrygroup-create-card/entrygroup-create-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entrygroup-create-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './entrygroup-create-card.component.html',
+  styleUrl: './entrygroup-create-card.component.css'
+})
+export class EntrygroupCreateCardComponent {
+
+}
