16f7a66505fd1517cfdd7d83a4b3f733f39f71b8
Marius Damm <mariusdamm1010@gmail.com>
2025-01-04 13:52:52 +0100

    feat (web): update entrygroup create card
    remove uniqueness. Now an intake and a spending card are to be displayed
    
    Issue #35

--- web/src/app/screens/settings-screen/entrygroup-create-card/entrygroup-create-card.component.ts
+++ web/src/app/screens/settings-screen/entrygroup-create-card/entrygroup-create-card.component.ts
@@ -1,9 +1,7 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {NgClass, NgForOf} from "@angular/common";
 import {EntrygroupCreateModalComponent} from "../entrygroup-create-modal/entrygroup-create-modal.component";
 import {EntrygroupDto} from "../../../dtos/entrygroup-dto";
-import {AxiosService} from "../../../services/axios.service";
-import {AuthService} from "../../../services/auth.service";
 import {toggleIconRotate} from "../../../util";
 
 @Component({
@@ -11,39 +9,18 @@ import {toggleIconRotate} from "../../../util";
   standalone: true,
   imports: [
     NgForOf,
-    EntrygroupCreateModalComponent
+    EntrygroupCreateModalComponent,
+    NgClass
   ],
   templateUrl: './entrygroup-create-card.component.html',
   styleUrl: './entrygroup-create-card.component.css'
 })
-export class EntrygroupCreateCardComponent implements OnInit{
-  groups: EntrygroupDto[] = []
+export class EntrygroupCreateCardComponent {
+  @Input() groups: EntrygroupDto[] = []
+  @Input() isIntake!: boolean;
   @ViewChild('collapseIcon') collapseIcon!: ElementRef;
 
-  ngOnInit() {
-    this.fetchGroups();
-  }
-
-  constructor(
-    private readonly axiosService: AxiosService,
-    private readonly authService: AuthService,
-  ) {
-  }
-
-  fetchGroups() {
-    this.axiosService.request(
-      "GET",
-      "/entrygroup",
-      ""
-    ).then(response => {
-      return response.data;
-    }).then(groups => {
-      if (groups !== null)
-        this.groups = groups;
-    }).catch(error => {
-      if (error.response.status === 401)
-        this.authService.deleteJwtToken();
-    });
+  constructor() {
   }
 
   protected readonly toggleIconRotate = toggleIconRotate;
